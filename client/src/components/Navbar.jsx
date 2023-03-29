aec26d04fc42d693fc2e9e4c531a20bd6dfabba5
Roshanell Francisco <96213223+Roshanell@users.noreply.github.com>
2023-03-29 10:46:16 -0500

    nav bar componenet updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
 import Logo from "../assets/BlueTechtonicaWord.png";
 
 function MyNavBar(props) {
